abd5b8f884d7868ddac68be93df32a9bb3baf8fc
FantiGA <98534535+FantiGA@users.noreply.github.com>
2022-05-25 19:06:38 +0800

    rewrite in functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
-export default function App () {
-	return <Outlet />
-}
+// export default function App () {
+// 	return <Outlet />
+// }
+
+/**
+ * 改函数式写法
+ */
+const App: React.FC = () => <Outlet />
+
+export default App
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,8 @@ import { useNavigate } from 'react-router-dom'
 import '../scss/home.scss'
 /* *
  * 首页
- * */ 
-export default function Home () {
+ * */
+/* export default function Home () {
     let navigate = useNavigate()
     return (
         <div className="home">
@@ -33,4 +33,39 @@ export default function Home () {
             </footer>
         </div>
     )
+} */
+
+/**
+ * 改函数式写法
+ */
+const Home: React.FC = () => {
+    let navigate = useNavigate()
+    return (
+        <div className="home">
+            <header>
+                <h1 className="hd-l">Welcome to</h1>
+                <div className="hd-r">
+                    <span className="square square_1">2</span>
+                    <span className="square square_2">0</span>
+                    <span className="square square_3">4</span>
+                    <span className="square square_4">8</span>
+                </div>
+            </header>
+
+            <ul>
+                <li>
+                    <button className="btn" onClick={() => navigate('/game')}>Start Game</button>
+                </li>
+                <li>
+                    <button className="btn" onClick={() => navigate('/ranking')}>Check Ranking</button>
+                </li>
+            </ul>
+
+            <footer>
+                Written in React and Typescript by Swan Cai on May 4th, 2022.
+            </footer>
+        </div>
+    )
 }
+
+export default Home
