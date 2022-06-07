98df5bda74978a1390f8eba79b08726d161dac83
FantiGA <98534535+FantiGA@users.noreply.github.com>
2022-06-07 15:24:24 +0800

    add CORS support

--- server/query.py
+++ server/query.py
@@ -1,7 +1,7 @@
 '''
 Author: fantiga
 Date: 2022-06-02 18:05:59
-LastEditTime: 2022-06-06 20:30:12
+LastEditTime: 2022-06-07 15:22:34
 LastEditors: fantiga
 Description: 
 FilePath: /2048-react/server/query.py
@@ -11,14 +11,33 @@ from flask import Flask, request
 import os
 import sqlite3
 import time
+from flask_cors import CORS
 
 app = Flask(__name__)
 
+""" 
+    跨域支持
+ """
+CORS(app, supports_credentials=True, origins='*', methods='*', allow_headers='*')
+
+# app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+# def after_request(resp):
+#     resp.headers['Access-Control-Allow-Origin'] = '*'
+#     return resp
+
+
+# app.after_request(after_request)
 
 class Db():
+    """ 
+        用于操作数据库的封装
+    """
+
     def __init__(self) -> None:
         # 数据库文件
-        self.db_file = 'bs_2048.db'
+        self.db_file = r'bs_2048.db'
         self.db = os.path.join(os.path.dirname(__file__), self.db_file)
         # 连接到SQLite数据库
         # 如果文件不存在，会自动在当前目录创建
@@ -26,15 +45,19 @@ class Db():
         # 创建一个Cursor
         self.cur = self.conn.cursor()
 
-    # 插入记录并返回新id
     def insertTableData(self, sql):
+        """ 
+            插入记录并返回新id
+        """
         self.cur.execute(sql)
         lastrowid = self.cur.lastrowid
         self.conn.commit()
         return lastrowid
 
-    # 查询并返回记录
     def selectTableData(self, sql):
+        """ 
+            查询并返回记录
+        """
         self.cur.execute(sql)
         self.conn.commit()
         # 数据集
@@ -43,21 +66,59 @@ class Db():
         get_table_fields = self.cur.description
         return [get_table_fields, get_table_data]
 
-    # 查询并返回排名
     def selectRankNum(self, sql):
+        """ 
+            查询并返回排名
+        """
         self.cur.execute(sql)
         self.conn.commit()
         # 单条数据
         return self.cur.fetchone()
 
-    # 关闭数据库
     def closeDatabase(self):
+        """ 
+            关闭数据库
+        """
         self.cur.close()
         self.conn.close()
 
 
+def getAll():
+    """ 
+        获取前10名
+    """
+    db = Db()
+    # 拼接sql语句
+    sql = "SELECT a.id, a.user_name, a.user_score, a.created_time FROM \"main\".\"bs_2048_server_rankdata\" a JOIN (SELECT * FROM \"main\".\"bs_2048_server_rankdata\" ORDER BY created_time DESC LIMIT 9999) b ON a.id=b.id GROUP BY a.user_score ORDER BY a.user_score DESC LIMIT 10"
+    fields, fetch_data = db.selectTableData(sql)
 
-@app.route('/query', methods=['POST'])
+    # 定义表结构的列表
+    column_list = []
+
+    # 提取字段，追加到列表中
+    for i in fields:
+        column_list.append(i[0])
+
+    # 按行将数据存入数组中，并转换为json格式
+    #column_size = len(column_list)
+    # 定义存储sql数据的list
+    sql_list = []
+    # 按行对sql数据进行循环
+    for rank_num, row in enumerate(fetch_data):
+        # 表数据与表结构对应 存入字典中
+        result = {}
+        result['rank_num'] = rank_num + 1
+        for i in range(len(row)):
+            result[column_list[i]] = row[i]
+        # 字典存入list中
+        sql_list.append(result)
+
+    db.closeDatabase()
+
+    return sql_list
+
+
+@app.route(r'/query', methods=['POST'])
 def query():
     # 接收到的数据
     action = request.form['action']
@@ -66,7 +127,7 @@ def query():
     if action == 'add':
         user_name = request.form['user_name']
         user_score = int(request.form['user_score'])
-        now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        now = time.strftime(r"%Y-%m-%d %H:%M:%S", time.localtime())
         db = Db()
 
         # 拼接sql语句
@@ -103,36 +164,5 @@ def query():
     return json
 
 
-def getAll():
-    db = Db()
-    # 拼接sql语句
-    sql = "SELECT a.id, a.user_name, a.user_score, a.created_time FROM \"main\".\"bs_2048_server_rankdata\" a JOIN (SELECT * FROM \"main\".\"bs_2048_server_rankdata\" ORDER BY created_time DESC LIMIT 9999) b ON a.id=b.id GROUP BY a.user_score ORDER BY a.user_score DESC LIMIT 10"
-    fields, fetch_data = db.selectTableData(sql)
-
-    # 定义表结构的列表
-    column_list = []
-
-    # 提取字段，追加到列表中
-    for i in fields:
-        column_list.append(i[0])
-
-    # 按行将数据存入数组中，并转换为json格式
-    #column_size = len(column_list)
-    # 定义存储sql数据的list
-    sql_list = []
-    # 按行对sql数据进行循环
-    for rank_num, row in enumerate(fetch_data):
-        # 表数据与表结构对应 存入字典中
-        result = {}
-        result['rank_num'] = rank_num + 1
-        for i in range(len(row)):
-            result[column_list[i]] = row[i]
-        # 字典存入list中
-        sql_list.append(result)
-
-    db.closeDatabase()
-
-    return sql_list
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
--- server/wsgi.py
+++ server/wsgi.py
@@ -1,7 +1,7 @@
 '''
 Author: fantiga
 Date: 2022-06-06 14:52:26
-LastEditTime: 2022-06-06 16:10:23
+LastEditTime: 2022-06-07 14:56:51
 LastEditors: fantiga
 Description: 
 FilePath: /2048-react/server/wsgi.py
@@ -9,7 +9,7 @@ FilePath: /2048-react/server/wsgi.py
 
 from query import app as application
 import sys
-sys.path.insert(0, "/var/www/ued.team/")
+sys.path.insert(0, r"/var/www/ued.team/")
 
 if __name__ == "__main__":
     application.run()
