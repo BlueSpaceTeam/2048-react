04f53cc06c00638421c87a1b7cda3f5d41042d41
FantiGA <98534535+FantiGA@users.noreply.github.com>
2022-06-14 14:35:25 +0800

    Update wsgi.py

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,7 +1,7 @@
 '''
 Author: fantiga
 Date: 2022-06-06 14:52:26
-LastEditTime: 2022-06-07 14:56:51
+LastEditTime: 2022-06-14 14:34:10
 LastEditors: fantiga
 Description: 
 FilePath: /2048-react/server/wsgi.py
@@ -9,7 +9,7 @@ FilePath: /2048-react/server/wsgi.py
 
 from query import app as application
 import sys
-sys.path.insert(0, r"/var/www/ued.team/")
+sys.path.insert(0, r"/var/www/2048.ued.team/")
 
 if __name__ == "__main__":
     application.run()
