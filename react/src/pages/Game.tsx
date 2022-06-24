bf6ec9b5352c40ef1d62a6a8fa80e9611310975e
FantiGA <98534535+FantiGA@users.noreply.github.com>
2022-06-24 17:08:53 +0800

    Complete remarks

--- react/src/pages/Game.tsx
+++ react/src/pages/Game.tsx
@@ -1,33 +1,33 @@
 /*
  * @Author: swancai
  * @Date: 2022-05-24 16:58:00
- * @LastEditTime: 2022-06-24 16:54:35
- * @LastEditors: swancai
+ * @LastEditTime: 2022-06-24 17:06:56
+ * @LastEditors: fantiga
  * @Description: 
  * @FilePath: /2048-react/react/src/pages/Game.tsx
  */
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
-import LogoButton from '@components/Game/LogoButton'
-import Score from '@components/Game/Score'
-import Board from '@components/Game/Board'
-import GameButton from '@components/Game/GameButton'
+import LogoButton from '@components/Game/LogoButton';
+import Score from '@components/Game/Score';
+import Board from '@components/Game/Board';
+import GameButton from '@components/Game/GameButton';
 
-import Portal from '@components/common/Portal'
-import ResultModal from '@components/Game/ResultModal'
+import Portal from '@components/common/Portal';
+import ResultModal from '@components/Game/ResultModal';
 
-import { usePrevious } from '@utils/hooks'
-import { startTimeDiff, endTimeDiff } from '@utils/timer'
+import { usePrevious } from '@utils/hooks';
+import { startTimeDiff, endTimeDiff } from '@utils/timer';
 
 import {
 	genNewNum,
 	checkPerpendicularDirPossibility,
 	getMoveResult
-} from '@utils/movingLogic'
+} from '@utils/movingLogic';
 
 // 引入声音模块
-import useSound from 'use-sound'
-import beepSfx from '@/sfx/s1.mp3'
+import useSound from 'use-sound';
+import beepSfx from '@/sfx/s1.mp3';
 
 import {
 	Direction,
@@ -35,160 +35,160 @@ import {
 	STORAGE_GAME_HISTORY,
 	STORAGE_GAME_SCORES,
 	STORAGE_BEST_SCORE,
-} from '@utils/constants'
+} from '@utils/constants';
 
-import '@scss/game.scss'
+import '@scss/game.scss';
 
 
 interface IGame { }
 
 // 初始化的方块历史
-const initialHistory = { squares: new Array(16).fill(0) }
+const initialHistory = { squares: new Array(16).fill(0) };
 
 /**
  * 游戏
  */
 const Game: React.FC<IGame> = (props) => {
 	// 游戏移动记录：[初始记录,(上一步记录,)当前记录]。只记录最近操作的2步，故包括初始数组最多长度为3
-	const [history, setHistory] = useState<IAHistoryOfSquares[]>([initialHistory])
+	const [history, setHistory] = useState<IAHistoryOfSquares[]>([initialHistory]);
 	// 随机生产的数字所处数组的下标
-	const [randomNumIdx, setRandomNumIdx] = useState<number>(-1)
+	const [randomNumIdx, setRandomNumIdx] = useState<number>(-1);
 	// 分数：[(上一步分数,)当前分数]
-	const [scores, setScores] = useState<number[]>([0])
+	const [scores, setScores] = useState<number[]>([0]);
 	// 总耗时（毫秒）
-	const [timeSpent, setTimeSpent] = useState<number>(0)
+	const [timeSpent, setTimeSpent] = useState<number>(0);
 	// 游戏是否结束
-	const [isOver, setIsOver] = useState<boolean>(false)
+	const [isOver, setIsOver] = useState<boolean>(false);
 	// 最高分
-	const [bestScore, setBestScore] = useState<number>(Number(localStorage.getItem(STORAGE_BEST_SCORE) || 0))
+	const [bestScore, setBestScore] = useState<number>(Number(localStorage.getItem(STORAGE_BEST_SCORE) || 0));
 	// 最高分原始值
-	const preBestScore: number = usePrevious(bestScore) || 0
-	// 音效播放
-	const [isMuted, setMuted] = useState<boolean>(false)
-	const [play] = useSound(beepSfx, { soundEnabled: !isMuted })
+	const preBestScore: number = usePrevious(bestScore) || 0;
+	// 音效播放，isMuted默认false（不静音）
+	const [isMuted, setMuted] = useState<boolean>(false);
+	const [playSfx] = useSound(beepSfx, { soundEnabled: !isMuted });
 
 	// btn-undo是否可点击
-	const disabledUndo: boolean = isOver || history.length < 3
+	const disabledUndo: boolean = isOver || history.length < 3;
 
 	// 从垂直于当前操作方向的方向来检查没有机会合并，game over的回调处理
 	const gameOverCallback: () => void = () => {
 		// 结束时间差计算
-		setTimeSpent(endTimeDiff())
+		setTimeSpent(endTimeDiff());
 
-		const curScore: number = scores[scores.length - 1]
+		const curScore: number = scores[scores.length - 1];
 		if (curScore > preBestScore) {
-			setBestScore(curScore)
-			localStorage.setItem(STORAGE_BEST_SCORE, curScore + '')
+			setBestScore(curScore);
+			localStorage.setItem(STORAGE_BEST_SCORE, curScore + '');
 		}
-		setIsOver(true)
-		localStorage.removeItem(STORAGE_GAME_HISTORY)
-		localStorage.removeItem(STORAGE_GAME_SCORES)
-	}
+		setIsOver(true);
+		localStorage.removeItem(STORAGE_GAME_HISTORY);
+		localStorage.removeItem(STORAGE_GAME_SCORES);
+	};
 
 	/**
 	 * 移动处理
 	 * 
 	 **/
 	function handleMove(direction: Direction): void {
-		if (isOver) return
+		if (isOver) return;
 
-		const currentHistory: IAHistoryOfSquares = history[history.length - 1]
+		const currentHistory: IAHistoryOfSquares = history[history.length - 1];
 		// 生成移动结果及得分
-		const { arr = [], scoreDelta = 0 } = getMoveResult(direction, currentHistory.squares!.slice())
+		const { arr = [], scoreDelta = 0 } = getMoveResult(direction, currentHistory.squares!.slice());
 		// 如果完全相同，则不发生变化
 		if (JSON.stringify(currentHistory.squares) === JSON.stringify(arr)) {
 			// 从垂直于当前操作方向的方向来检查是否仍然有机会合并，如果没有则认为游戏结束
 			if (!checkPerpendicularDirPossibility(direction, arr)) {
-				gameOverCallback()
+				gameOverCallback();
 			}
-			return
+			return;
 		} else {
 			// 从垂直于当前操作方向的方向来检查是否仍然有机会合并，如果没有则认为游戏结束
 			if (!checkPerpendicularDirPossibility(direction, arr)) {
-				gameOverCallback()
-				return
+				gameOverCallback();
+				return;
 			}
 		}
 
 		// 播放音效
-		play()
+		playSfx();
 		// 随机生成方块并替换当前历史
-		const { idx: idxNew, arr: squareNew } = genNewNum(arr)
-		const newHistory: IAHistoryOfSquares[] = history.slice()
+		const { idx: idxNew, arr: squareNew } = genNewNum(arr);
+		const newHistory: IAHistoryOfSquares[] = history.slice();
 		// 只保留初始记录 和 最近2次的操作记录
 		if (newHistory.length > 3) {
-			newHistory.splice(1, 1)
-		} 
-		newHistory.splice(newHistory.length, 1, { squares: squareNew })
-		setHistory(newHistory)
-		setRandomNumIdx(idxNew)
+			newHistory.splice(1, 1);
+		}
+		newHistory.splice(newHistory.length, 1, { squares: squareNew });
+		setHistory(newHistory);
+		setRandomNumIdx(idxNew);
 		// 计算分数
 		if (scoreDelta) {
 			setScores((scores: string | any[]) => {
-				const prevScore = scores[scores.length - 1]
-				const nowScore = prevScore + scoreDelta
-				return [prevScore, nowScore]
-			})
+				const prevScore = scores[scores.length - 1];
+				const nowScore = prevScore + scoreDelta;
+				return [prevScore, nowScore];
+			});
 		}
 	}
 
 	// 历史记录重新设置
 	const resetHistory: () => void = () => {
-		const { idx: idxNew, arr: squareNew } = genNewNum(initialHistory.squares)
-		setHistory([initialHistory, { squares: squareNew }])
-		setRandomNumIdx(idxNew)
-	}
+		const { idx: idxNew, arr: squareNew } = genNewNum(initialHistory.squares);
+		setHistory([initialHistory, { squares: squareNew }]);
+		setRandomNumIdx(idxNew);
+	};
 
 	// 开始游戏
 	const startGame: () => void = () => {
 		// 开始时间差计算
-		startTimeDiff()
-		resetHistory()
-		setScores([0])
-	}
+		startTimeDiff();
+		resetHistory();
+		setScores([0]);
+	};
 	// 撤销上一步
 	const undoGame: () => void = () => {
 		if (history.length > 2) { // 除了初始数组外，存在上一步方可撤销
-			setHistory((oldHistory: IAHistoryOfSquares[]) => oldHistory.slice(0, oldHistory.length - 1))
-			setScores((scores: number[]) => scores.slice(0, 1))
+			setHistory((oldHistory: IAHistoryOfSquares[]) => oldHistory.slice(0, oldHistory.length - 1));
+			setScores((scores: number[]) => scores.slice(0, 1));
 		}
-	}
+	};
 
 	// 初始化游戏界面-board及计时
 	useEffect(() => {
 		// 开始时间差计算
-		startTimeDiff()
-		const StorageHistoryStr: string = localStorage.getItem(STORAGE_GAME_HISTORY) || ''
+		startTimeDiff();
+		const StorageHistoryStr: string = localStorage.getItem(STORAGE_GAME_HISTORY) || '';
 		if (StorageHistoryStr) {
-			const SHistory: IAHistoryOfSquares[] = JSON.parse(StorageHistoryStr)
+			const SHistory: IAHistoryOfSquares[] = JSON.parse(StorageHistoryStr);
 			if (SHistory.length > 1) {
-				setHistory(SHistory)
+				setHistory(SHistory);
 			} else {
-				resetHistory()
+				resetHistory();
 			}
 		} else {
-			resetHistory()
+			resetHistory();
 		}
-	}, [])
+	}, []);
 	// 初始化游戏界面-分数
 	useEffect(() => {
-		const StorageScoresStr: string = localStorage.getItem(STORAGE_GAME_SCORES) || ''
+		const StorageScoresStr: string = localStorage.getItem(STORAGE_GAME_SCORES) || '';
 		if (StorageScoresStr) {
-			const SScores: number[] = JSON.parse(StorageScoresStr)
+			const SScores: number[] = JSON.parse(StorageScoresStr);
 			if (SScores.length > 1) {
-				setScores(SScores)
+				setScores(SScores);
 			}
 		}
-	}, [])
+	}, []);
 
 	// 记录操作至缓存
 	useEffect(() => {
-		localStorage.setItem(STORAGE_GAME_HISTORY, JSON.stringify(history))
-	}, [history])
+		localStorage.setItem(STORAGE_GAME_HISTORY, JSON.stringify(history));
+	}, [history]);
 	// 记录操作至缓存
 	useEffect(() => {
-		localStorage.setItem(STORAGE_GAME_SCORES, JSON.stringify(scores))
-	}, [scores])
+		localStorage.setItem(STORAGE_GAME_SCORES, JSON.stringify(scores));
+	}, [scores]);
 
 	// 控制Modal
 	const ModalUI: JSX.Element | null = isOver ? (
@@ -203,7 +203,7 @@ const Game: React.FC<IGame> = (props) => {
 			/>
 		</Portal>
 	)
-		: null
+		: null;
 
 	return (
 		<div className="game">
@@ -226,7 +226,7 @@ const Game: React.FC<IGame> = (props) => {
 
 			{ModalUI}
 		</div>
-	)
-}
+	);
+};
 
-export default Game
+export default Game;
--- react/src/utils/timer.tsx
+++ react/src/utils/timer.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: fantiga
  * @Date: 2022-06-23 16:16:50
- * @LastEditTime: 2022-06-23 19:09:33
+ * @LastEditTime: 2022-06-24 12:18:30
  * @LastEditors: fantiga
  * @Description: 
  * @FilePath: /2048-react/react/src/utils/timer.tsx
@@ -15,20 +15,18 @@
 const getNow = (): number => {
     if (!Date.now) {
         Date.now = function now() {
-            return new Date().getTime()
-        }
+            return new Date().getTime();
+        };
     }
-
-    return Date.now()
-}
-
+    return Date.now();
+};
 
 /**
  * @name: startTimeDiff
  * @description: 开始时间差计算。生成一个当前时间的Unix时间戳，写入sessionStorage中。
  * @return {*}
  */
-export const startTimeDiff = (): void => sessionStorage.setItem('timer', String(getNow()))
+export const startTimeDiff = (): void => sessionStorage.setItem('timer', String(getNow()));
 
 /**
  * @name: endTimeDiff
@@ -36,8 +34,8 @@ export const startTimeDiff = (): void => sessionStorage.setItem('timer', String(
  * @return {*} number   总耗时
  */
 export const endTimeDiff = (): number => {
-    let timer: number | null = Number(sessionStorage.getItem('timer'))
-    timer = getNow() - timer
-    sessionStorage.removeItem('timer')
-    return timer
-}
+    let timer: number | null = Number(sessionStorage.getItem('timer'));
+    timer = getNow() - timer;
+    sessionStorage.removeItem('timer');
+    return timer;
+};
