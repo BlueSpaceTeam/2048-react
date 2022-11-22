16761c2d3f6b26806aa2e99677e89ecd5d8ed6ce
FantiGA <98534535+FantiGA@users.noreply.github.com>
2022-11-23 01:57:52 +0900

    Update webpack.config.js

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -2,43 +2,43 @@
 
 const path = require('path');
 
-// 引入HTML插件
+// Import HTML plugin / 引入HTML插件
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
-// 引入clean插件
+// Introduce the clean plugin / 引入clean插件
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 // const { SourceMapDevToolPlugin } = require('webpack')
 
 const config = {
-  // 指定打包模式：'none' | 'development' | 'production'
+  // Specify the packaging method: 'none' | 'development' | 'production' / 指定打包模式
   mode: 'production',
-  // 指定入口文件
+  // Specify the entry file / 指定入口文件
   entry: './src/index.tsx',
-  // 指定打包文件所在目录
+  // Specify the directory where the package file is located / 指定打包文件所在目录
   output: {
-    // 指定打包文件的目录
+    // Specify the directory of the package file / 指定打包文件的目录
     path: path.resolve(__dirname, 'dist'),
-    // 打包后的文件名
+    // Packaged file name / 打包后的文件名
     filename: 'bundle.js',
     environment: {
-      // 不使用箭头函数
+      // Not using arrow functions / 不使用箭头函数
       // arrowFunction: false,
-      // 不使用const（为了兼容IE10）
+      // Do not use const (for compatibility with IE10) / 不使用const（为了兼容IE10）
       // const: false,
     },
     publicPath: '/',
   },
-  // 指定webpack打包时要用的模块
+  // Specify the modules to be used when webpack packs / 指定webpack打包时要用的模块
   module: {
-    // 指定要加载的规则
+    // Specifies the rules to load / 指定要加载的规则
     rules: [
       {
-        // test指定的是规则生效的文件
+        // 'test' specifies the file in which the rule takes effect / 'test'指定的是规则生效的文件
         test: /\.js$/,
-        // 要使用的loader
+        // Loader to use / 要使用的loader
         use: ['babel-loader', 'source-map-loader'],
-        // 要排除的文件
+        // Files to exclude / 要排除的文件
         exclude: /node_modules/,
       },
       {
@@ -49,26 +49,26 @@ const config = {
       {
         test: /\.ts(x)?$/,
         use: [
-          // 配置babel
+          // Configure babel / 配置babel
           {
-            // 指定加载器
+            // Specified loader / 指定加载器
             loader: 'babel-loader',
-            // 设置babel
+            // Set up babel / 设置babel
             options: {
-              // 设置预定义的环境
+              // Set up a predefined environment / 设置预定义的环境
               presets: [
                 [
-                  // 指定环境的插件
+                  // Plugins for specific environments / 指定环境的插件
                   '@babel/preset-env',
-                  // 配置信息
+                  // Configuration information / 配置信息
                   {
-                    // 要兼容的浏览器
+                    // Compatible browser / 要兼容的浏览器
                     targets: {
                       chrome: '88',
                     },
-                    // 指定corejs的版本
+                    // Specify the version of corejs / 指定corejs的版本
                     corejs: '3',
-                    // 使用corejs的方式，'usage'表示按需加载
+                    // The way to use corejs, 'usage' means loading on demand / 使用corejs的方式，'usage'表示按需加载
                     useBuiltIns: 'usage',
                   },
                 ],
@@ -93,13 +93,13 @@ const config = {
         ],
         include: /\.module\.css$/,
       },
-      // 设置sass文件处理
+      // Set up sass file handling / 设置sass文件处理
       {
         test: /\.s[ac]ss$/i,
         use: [
           'style-loader',
           'css-loader',
-          // 引入postcss
+          // Import postcss / 引入postcss
           {
             loader: 'postcss-loader',
             options: {
@@ -115,11 +115,11 @@ const config = {
               },
             },
           },
-          // 将 Sass 编译成 CSS
+          // Compile Sass to CSS / 将 Sass 编译成 CSS
           'sass-loader',
         ],
       },
-      // 设置svg文件处理
+      // Set up svg file handling / 设置svg文件处理
       {
         test: /\.svg$/,
         exclude: /node_modules/,
@@ -132,7 +132,7 @@ const config = {
           },
         ],
       },
-      // 设置mp3文件处理
+      // Set up mp3 file handling / 设置mp3文件处理
       {
         test: /\.mp3$/,
         use: [
@@ -143,7 +143,7 @@ const config = {
       },
     ],
   },
-  // 设置哪些文件可以用于引用模块
+  // Set which files can be used to reference modules / 设置哪些文件可以用于引用模块
   resolve: {
     extensions: ['', '.js', '.jsx', '.ts', '.tsx', '.webpack.js', '.web.js'],
     alias: {
@@ -153,32 +153,32 @@ const config = {
       '@utils': path.resolve(__dirname, './src/utils'),
     },
   },
-  // 配置webpack插件
+  // Configure the webpack plugin / 配置webpack插件
   plugins: [
     new CleanWebpackPlugin(),
     new HTMLWebpackPlugin({
       // title: '2048 - React',
-      // filename: './dist/index.html', // 配置生成的HTML文件名称
-      template: path.resolve(__dirname, 'public', 'index.html'), // 生成HTML文件所需要的模板文件
+      // filename: './dist/index.html', // Configure the generated HTML file name / 配置生成的HTML文件名称
+      template: path.resolve(__dirname, 'public', 'index.html'), // Template files required to generate HTML files / 生成HTML文件所需要的模板文件
       minify: {
-        removeComments: true, // 打包后移除html文件中的注释
+        removeComments: true, // Remove comments in HTML files after packaging / 打包后移除HTML文件中的注释
       },
-      // 导入ico文件
+      // Import ico file / 导入ico文件
       favicon: path.resolve(__dirname, 'public', 'favicon.ico'),
-      // 导入manifest文件
+      // Import the manifest file / 导入manifest文件
       // manifest: path.resolve(__dirname, 'public', 'manifest.json'),
     }),
     // new SourceMapDevToolPlugin({
     //     filename: '[file].map',
     // }),
   ],
-  // 性能提示
+  // Performance Tips / 性能提示
   performance: {
-    // 打开/关闭提示
+    // Turn on/off hints / 打开/关闭提示
     hints: false,
-    // 何时根据最大入口点大小（以字节为单位）发出性能提示
+    // When to emit a performance hint based on the maximum entry point size in bytes / 何时根据最大入口点大小（以字节为单位）发出性能提示
     maxEntrypointSize: 512000,
-    // 何时根据单个资产大小（以字节为单位）发出性能提示
+    // When to issue performance hints based on individual asset size in bytes / 何时根据单个资产大小（以字节为单位）发出性能提示
     maxAssetSize: 512000,
   },
   devServer: {
@@ -191,7 +191,7 @@ const config = {
         changeOrigin: true,
       },
     },
-    // 解决react-router-dom中使用BrowserRouter刷新后404的问题
+    // Solve the problem of 404 after refreshing with BrowserRouter in react-router-dom / 解决react-router-dom中使用BrowserRouter刷新后404的问题
     historyApiFallback: true,
   },
 };
